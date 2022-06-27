f3fe555d2fff685037f3f54aa4d932f833f99889
Santiago Sáenz <santiago.saenz@flip.org.co>
2022-06-27 09:29:31 -0500

    Modified Places API request parameters

--- lib/mapsClient.ts
+++ lib/mapsClient.ts
@@ -1,7 +1,5 @@
 import * as dotenv from "dotenv";
-import {
-  Client
-} from "@googlemaps/google-maps-services-js";
+import { Client } from "@googlemaps/google-maps-services-js";
 
 dotenv.config();
 
@@ -10,17 +8,20 @@ export const maps = new Client({});
 type valid_places = "restaurant" | "museum" | "night_club" | "cafe";
 
 async function getNearbyPlaces(
+  keyword: string,
   location: [number, number],
   radius: number,
   type: valid_places,
-  maxprice: 0 | 1 | 2 | 3 | 4 | undefined = undefined,
-  pagetoken: string | undefined = undefined,
+  minPrice: 0 | 1 | 2 | 3 | 4 | undefined = undefined,
+  maxprice: 0 | 1 | 2 | 3 | 4 | undefined = undefined
 ) {
   const params = {
+    keyword,
     location,
     key: process.env.MAPS_API_KEY as string,
     radius,
     type,
+    minPrice,
     maxprice,
   };
   let {
@@ -34,10 +35,3 @@ async function getNearbyPlaces(
   // }
   return places;
 }
-
-// async function main() {
-
-//     const places = await getNearbyPlaces([4.676791724087879, -74.04847963497164],1000, "cafe");
-//     console.log(places.map(({name}) => name));
-// }
-// main();
