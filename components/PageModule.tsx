7cd96ea8392bbcf4721bdd5475faeddffe9041cd
Santiago Sáenz <santiago.saenz@flip.org.co>
2022-06-28 12:20:44 -0500

    Fix horizontal sizing on cardList component

--- components/PageModule.tsx
+++ components/PageModule.tsx
@@ -8,7 +8,7 @@ type Props = {
 const PageModule: React.FC<Props> = (props) => {
   return (
     <main className="flex flex-col justify-center min-h-screen">
-      <div className="fixed top-0 bg-gradient-to-b from-conifer-500 to-apple-400 min-w-full h-80 z-0 rounded-b-[40px]"></div>
+      <div className="fixed top-0 bg-gradient-to-b from-conifer-500 to-apple-400 min-w-full h-60 sm:h-28 sm:rounded-none z-0 rounded-b-[40px]"></div>
       <div className="z-20 fixed top-0">
         <h1 className="font-nunito mt-6 ml-6 text-4xl font-extrabold text-white sm:text-6xl ">
           {props.title}
