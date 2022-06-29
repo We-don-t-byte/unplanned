9dbcbb2de58d295e80c7c4e2f8a69694177a6a84
Santiago Sáenz <santiago.saenz@flip.org.co>
2022-06-29 03:41:34 -0500

    Added props to Tindercard type

--- components/Swiper.tsx
+++ components/Swiper.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import TinderCard from "react-tinder-card";
 import PlaceCard from "../components/PlaceCard";
 
 //...
+
+type TinderCardProps = Parameters<typeof TinderCard>[0] & {
+  children?: ReactNode;
+};
+
+const UnplannedCard: React.FC<TinderCardProps> = TinderCard;
+
+type Props = {
+  places: string[];
+};
 
 const Swiper = () => {
   const handleSwipe = (d: any) => {
@@ -10,7 +20,7 @@ const Swiper = () => {
   };
   return (
     <div className="">
-      <TinderCard preventSwipe={["up", "down"]}>
+      <UnplannedCard preventSwipe={["up", "down"]}>
         <PlaceCard
           name="Storia D'amore"
           address="Carrera 13 # 82 -36
@@ -18,7 +28,7 @@ const Swiper = () => {
           image="https://restaurantestoriadamore.com/media/82-7.jpg"
           tags={["Restaurante", "Café", "Bar"]}
         />
-      </TinderCard>
+      </UnplannedCard>
     </div>
   );
 };
